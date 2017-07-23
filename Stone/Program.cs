39c2940ec36e5bdbf81b77decd77c9c2e07849a2
Frederick-S <mao_xiaodan@hotmail.com>
2017-07-23 15:33:45 +0800

    Init

---
+++ Stone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

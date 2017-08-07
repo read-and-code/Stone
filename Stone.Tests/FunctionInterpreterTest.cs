08846385ab47e65a9ba939eac64a65f1344c5192
Frederick-S <mao_xiaodan@hotmail.com>
2017-08-07 11:58:45 +0800

    Add fib test

--- Stone.Tests/FunctionInterpreterTest.cs
+++ Stone.Tests/FunctionInterpreterTest.cs
@@ -11,9 +11,30 @@ namespace Stone.Tests
     public class FunctionInterpreterTest
     {
         [Fact]
-        public void Interpret()
+        public void Sum()
         {
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "samples/sum.txt");
+            Lexer lexer = new Lexer(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            FunctionParser functionParser = new FunctionParser();
+            IEnvironment environment = new NestedEnvironment();
+
+            while (lexer.Peek(0) != Token.EOF)
+            {
+                ASTree asTree = functionParser.Parse(lexer);
+
+                if (!(asTree is NullStatement))
+                {
+                    object result = asTree.Eval(environment);
+
+                    System.Console.WriteLine(result);
+                }
+            }
+        }
+
+        [Fact]
+        public void Fib()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "samples/fib.txt");
             Lexer lexer = new Lexer(new FileStream(filePath, FileMode.Open, FileAccess.Read));
             FunctionParser functionParser = new FunctionParser();
             IEnvironment environment = new NestedEnvironment();

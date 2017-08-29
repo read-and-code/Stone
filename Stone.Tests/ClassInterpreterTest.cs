f81e3ee9b9afc9456db858ebb578265261db3a28
Frederick-S <mao_xiaodan@hotmail.com>
2017-08-29 17:32:18 +0800

    Update fib test

--- Stone.Tests/ClassInterpreterTest.cs
+++ Stone.Tests/ClassInterpreterTest.cs
@@ -32,5 +32,28 @@ namespace Stone.Tests
                 }
             }
         }
+
+        [Fact]
+        public static void Fib()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "samples/class2.st");
+            Lexer lexer = new Lexer(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            BasicParser basicParser = new BasicParser();
+            IEnvironment environment = new Environment();
+
+            while (lexer.Peek(0) != Token.EOF)
+            {
+                ASTNode astNode = basicParser.Parse(lexer);
+
+                if (!(astNode is NullStatement))
+                {
+                    astNode.Lookup(environment.SymbolTable);
+
+                    object result = astNode.Eval(environment);
+
+                    System.Console.WriteLine(result);
+                }
+            }
+        }
     }
 }

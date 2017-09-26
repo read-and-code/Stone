7bcc17953c87ba931945f34ac1ab44d01f5b2601
Frederick-S <mao_xiaodan@hotmail.com>
2017-09-26 15:46:15 +0800

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -2,10 +2,10 @@
 
 # Install OpenCover and ReportGenerator, and save the path to their executables.
 nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
-nuget install -Verbosity quiet -OutputDirectory packages -Version 2.5.8 ReportGenerator
+nuget install -Verbosity quiet -OutputDirectory packages -Version 3.0.0 ReportGenerator
 
 OPENCOVER=$PWD/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
-REPORTGENERATOR=$PWD/packages/ReportGenerator.2.5.8/tools/ReportGenerator.exe
+REPORTGENERATOR=$PWD/packages/ReportGenerator.3.0.0/tools/ReportGenerator.exe
 
 coverage=./coverage
 mkdir $coverage

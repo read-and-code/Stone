be4f0c65ff74144256596c3912ee010db3809943
Frederick-S <mao_xiaodan@hotmail.com>
2017-09-27 13:56:05 +0800

    Update tests

--- Stone.Tests/LexerTest.cs
+++ Stone.Tests/LexerTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using Stone;
 using Stone.Tokens;
@@ -10,16 +11,28 @@ namespace Stone.Tests
         [Fact]
         public void ReadToken()
         {
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "samples/lexer.st");
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "samples/lexer_assignment.st");
             Lexer lexer = new Lexer(new FileStream(filePath, FileMode.Open, FileAccess.Read));
             Token token = lexer.Read();
+            List<string> expectedTokens = new List<string>();
 
             while (token != Token.EOF)
             {
                 System.Console.WriteLine(token.ToString());
 
+                if (token.IsIdentifier || token.IsString)
+                {
+                    expectedTokens.Add(token.Text);
+                }
+                else if (token.IsNumber)
+                {
+                    expectedTokens.Add(token.Number.ToString());
+                }
+
                 token = lexer.Read();
             }
+
+            Assert.Equal(new List<string> { "i", "=", "1", "\\n" }, expectedTokens);
         }
     }
 }
